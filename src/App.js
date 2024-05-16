e35dc9366dc278237ec8702281e3073c008d595d
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-05-16 11:11:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO EASYDEPLOY
+          WELCOME TO THE NEW WORLD
         </p>
         <a
           className="App-link"
